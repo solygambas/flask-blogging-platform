3b94d4de5069a50cc9bbc070a851c8003bb736ef
solygambas <solygambas@gmail.com>
2020-11-28 14:29:28 +0100

    add REST api

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite') 
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
@@ -37,7 +37,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite') 
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
